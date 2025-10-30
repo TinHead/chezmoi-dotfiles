e5f42ac5cbb018850a238d276e95963fa99e370e
TinHead <razlix77@gmail.com>
2025-10-30 21:08:17 +0200

    add more to ssh config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 starship init fish | source
+/home/razvan/.local/bin/mise activate fish | source
 fish_add_path ~/.local/bin
 
 /var/home/razvan/.local/bin/mise activate fish | source # added by https://mise.run/fish
